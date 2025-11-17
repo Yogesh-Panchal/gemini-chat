c16770cf17d6374328bab42d9b0716bf943a14ca
Yogesh <Yogeshppanchal96@gmail.com>
2025-11-17 14:33:48 +0530

    feat: refactor App component to use RecentSearch and QuestionAnswer components for improved structure and clarity

---
+++ src/components/RecentSearch.jsx
@@ -0,0 +1,27 @@
+function RecentSearch({recenthistory,setRecentHistory,setSelectedHistory}) {
+
+    const clearHistory = () => {
+      localStorage.removeItem('history');
+      setRecentHistory([]);      
+    }  
+
+  return (
+    <>
+        <div className='col-span-1 bg-zinc-800 h-screen pt-5'>
+            <h1 className='text-xl text-center text-white flex justify-center'>
+            <span>Recent History</span>
+            <button onClick={clearHistory} className='cursor-pointer'><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z" /></svg></button>
+            </h1>
+            <ul className='text-left overflow-auto border-zinc-700 text-white ml-1 text-sm text-white m-5'>
+            {
+                recenthistory && recenthistory.map((item) => (
+                <li  onClick={()=>setSelectedHistory(item)} key={item} className='border-b p-1 truncate cursor-pointer hover:bg-zinc-700 hover:text-zinc-200'>{item}</li>
+                ))
+            }
+            </ul>
+        </div>
+    </>
+  );
+}
+
+export default RecentSearch;
