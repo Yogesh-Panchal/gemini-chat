d9f9d775cde8ea1eb4cfdb157c776c92efd9d806
Yogesh <Yogeshppanchal96@gmail.com>
2025-11-17 13:39:10 +0530

    feat: add loading indicator and refactor askQuestion function for better state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { URL } from './constants';
 import Answer from './components/Answer';
 function App() {
@@ -7,8 +7,65 @@ function App() {
   const [result, setResult] = useState([]);
   const [recenthistory, setRecentHistory] = useState(JSON.parse(localStorage.getItem('history')));
   const [selectedHistory, setSelectedHistory] = useState('');
+  const [loader,setloader]=useState(false);
+  const scrollToAns= useRef('');
   
+//   let isLoading = false;
 
+// const askQuestion = async () => {
+//   if (isLoading) return;
+//   isLoading = true;
+
+//   if(!question && !selectedHistory){
+//       return false;
+//     }
+
+//     if(!question && !selectedHistory){
+//       return false;
+//     }
+
+//     if(question){
+//       if (localStorage.getItem('history')) {
+//         let history = JSON.parse(localStorage.getItem('history'));
+//         history = [question, ...history];
+//         localStorage.setItem('history', JSON.stringify(history));
+//         setRecentHistory(history);
+//       }
+//       else {
+//         localStorage.setItem('history', JSON.stringify([question]));
+//         setRecentHistory([question]);
+//       }
+//       //console.log(recenthistory)
+//     }
+
+//     const payloadData =question ? question:selectedHistory;
+//     const payload = {
+//       "contents": [
+//         {"parts": [{
+//               "text": payloadData
+//             }]
+//         }
+//       ],
+//     }
+    
+//     let response = await fetch(URL, {
+//       method: "POST",
+//       body: JSON.stringify(payload)
+//     })
+
+//     response = await response.json();
+//     let dataString = response.candidates[0].content.parts[0].text;
+//     dataString = dataString.split("* ");
+//     dataString = dataString.map((item) => item.trim());
+
+//     setResult([...result, { type: 'q', text: question ? question:selectedHistory }, { type: 'a', text: dataString }]);
+//     setQuestion("");
+//     setTimeout(() => {
+//       scrollToAns.current.scrollTop=scrollToAns.current.scrollHeight;
+//     }, 500);
+
+//   isLoading = false;
+// };
   const askQuestion = async () => {
     if(!question && !selectedHistory){
       return false;
@@ -37,7 +94,7 @@ function App() {
         }
       ],
     }
-    
+    setloader(true);
     let response = await fetch(URL, {
       method: "POST",
       body: JSON.stringify(payload)
@@ -50,6 +107,10 @@ function App() {
 
     setResult([...result, { type: 'q', text: question ? question:selectedHistory }, { type: 'a', text: dataString }]);
     setQuestion("");
+    setTimeout(() => {
+      scrollToAns.current.scrollTop=scrollToAns.current.scrollHeight;
+    }, 500);
+    setloader(false);
   }
   const isEnter = (event) => {
     if (event.key === 'Enter') {
@@ -83,7 +144,19 @@ function App() {
         </ul>
       </div>
       <div className='col-span-4 p-10 '>
-        <div className='container h-110 overflow-y-auto '>
+        <h1 className='text-4xl bg-clip-text text-transparent bg-gradient-to-r from-pink-700 to-violet-700 text-center'>Hello User, Ask me Anything</h1>        
+           {
+             loader ?
+            <div role="status">
+              <svg aria-hidden="true" class="inline w-8 h-8 text-neutral-tertiary animate-spin fill-purple" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>:null
+           }
+        
+        <div ref ={scrollToAns} className='container h-110 overflow-y-auto bg-zinc-900'>
           <div className=' text-zinc-300 '>
             <ul>
               {
