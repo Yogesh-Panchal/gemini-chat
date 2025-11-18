a23ecb8e1caeb0bca9d361c07c3a4f792e530860
Yogesh <Yogeshppanchal96@gmail.com>
2025-11-18 18:24:03 +0530

    feat: add markdown rendering and syntax highlighting to Answer component
    - Added react-markdown and react-syntax-highlighter dependencies.
    - Implemented markdown rendering in the Answer component using ReactMarkdown.
    - Integrated syntax highlighting for code blocks using SyntaxHighlighter.
    - Updated state management and rendering logic to accommodate new features.

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,28 +1,56 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { checkHeading, replaceHeadingStarts } from '../helper';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+const Answer = ({ ans, totalResult, index, type }) => {
 
-const Answer = ({ ans,totalResult,index,type}) => {
-
-const [heading,setHeading]=useState(false);
-const [answer,setAnswer]=useState(ans);
-//  console.log(index);
+  const [heading, setHeading] = useState(false);
+  const [answer, setAnswer] = useState(ans);
+  //  console.log(index);
   useEffect(() => {
     if (checkHeading(ans)) {
-        setHeading(true);
-        setAnswer(replaceHeadingStarts(ans));
+      setHeading(true);
+      setAnswer(replaceHeadingStarts(ans));
     }
     //console.log(ans, checkHeading(ans));
   }, []);
 
+  const renderer = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+    
+
+        <SyntaxHighlighter
+          {...props}
+          //children={String(children).replace(/\n$/, '')}          
+          PreTag="div"
+          language={match[1]}
+          style={dark}             
+          >
+          {String(children).replace(/\n$/, '')}
+         </SyntaxHighlighter>
+        
+      ):(
+        <code {...props} className={className}>
+          {children}
+        </code>
+      )
+    }
+  }
+
   return (
     <>
-        {index === 0 && totalResult > 1 ? (
+      {index === 0 && totalResult > 1 ? (
         <span className="pt-1 text-xl block text-white">{answer}</span>
-        ) : heading && totalResult > 1 ? (
+      ) : heading && totalResult > 1 ? (
         <span className="pt-1 text-lg block text-white">{answer}</span>
-        ) : ( 
-        <span className={type == 'q'?'pl-1':'pl-5'}>{answer}</span>
-        )}
+      ) : (
+        <span className={type == 'q' ? 'pl-1' : 'pl-5'}>
+          <ReactMarkdown components={renderer}>{answer}</ReactMarkdown>
+        </span>
+      )}
     </>
   );
 };
